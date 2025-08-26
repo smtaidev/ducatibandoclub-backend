0fc1cc8c1f4777466471831fcfdbb4f31b19a58c
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-08-26 09:38:58 +0600

    fix error

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  transactionOptions: {
+    timeout: 10000, // 10 seconds timeout
+  },
+});
 export default prisma;
 
